471d3483a1c59976b74e7f2ca6ceb13b3cc3eed2
Phodal Huang <h@phodal.com>
2020-12-08 22:55:57 +0800

    feat: init serde yaml build

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -2,3 +2,25 @@ pub mod delimiter;
 pub mod lang_builtin;
 pub mod lang_default;
 pub mod type_alias;
+
+#[cfg(test)]
+mod tests {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct Point {
+        x: f64,
+        y: f64,
+    }
+
+    #[test]
+    fn should_convert_yaml() {
+        let point = Point { x: 1.0, y: 2.0 };
+
+        let s = serde_yaml::to_string(&point).unwrap();
+        assert_eq!(s, "---\nx: 1.0\ny: 2.0");
+
+        let deserialized_point: Point = serde_yaml::from_str(&s).unwrap();
+        assert_eq!(point, deserialized_point);
+    }
+}
